ea14e558f492cfa0d7820244bb0649e8b50a71fb
Angela Norris <cocoanorris@yahoo.com>
2019-11-05 16:00:01 -0500

    using context provider to make data variable

--- translate_context/src/components/App.js
+++ translate_context/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+
 
 export class App extends Component {
   state = { language: 'english'}
 
-  onLanguageChange =language => (
+  onLanguageChange =language => {(
     this.setState ({ language })
-  )
+  )}
 
   render() {
     return (
@@ -16,7 +18,9 @@ export class App extends Component {
           <i className="flag us" onClick={() => this.onLanguageChange('english')} />
           <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
         </div>
+        <LanguageContext.Provider value={this.state.language}>
         <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
