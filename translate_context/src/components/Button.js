13c2d254e7d8544adeadcb1e0172fb6cfb97aca9
Angela Norris <cocoanorris@yahoo.com>
2019-11-05 15:47:47 -0500

    creating context objects with default value

--- translate_context/src/components/Button.js
+++ translate_context/src/components/Button.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 export default class Button extends Component {
+
+  static contextType = LanguageContext;
+
   render() {
+    
+    console.log(this.context);
     return (
       <button className="ui button primary">
         Submit
