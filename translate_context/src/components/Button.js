daf53fcaeb555554f47c3fc5727c7b0506df63bf
Angela Norris <cocoanorris@yahoo.com>
2019-11-06 11:06:52 -0500

    using consumer with multiple contexts

--- translate_context/src/components/Button.js
+++ translate_context/src/components/Button.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 export default class Button extends Component {
 
-  static contextType = LanguageContext;
-
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
-      <button className="ui button primary">
-        {text}
-      </button>
+      <ColorContext.Consumer>
+      {(color) =>
+        <button className={`ui button ${color}`}>
+          <LanguageContext.Consumer>
+            {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+          </LanguageContext.Consumer>
+        </button>
+      }
+      </ColorContext.Consumer>
     );
   }
 }
