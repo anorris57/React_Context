7b6d251e7d9a617c1ca32b495f6f29f1ec8fa232
Angela Norris <cocoanorris@yahoo.com>
2019-11-08 18:16:33 -0500

    how to use context  instead of redux

--- translate_context/src/components/Button.js
+++ translate_context/src/components/Button.js
@@ -10,7 +10,7 @@ export default class Button extends Component {
       {(color) =>
         <button className={`ui button ${color}`}>
           <LanguageContext.Consumer>
-            {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+            {({language}) => language === 'english' ? 'Submit' : 'Voorleggen'}
           </LanguageContext.Consumer>
         </button>
       }
--- translate_context/src/components/Field.js
+++ translate_context/src/components/Field.js
@@ -7,7 +7,7 @@ export default class Field extends Component {
 
 
   render() {
-    const text = this.context === 'english' ? 'Name' : 'Naam';
+    const text = this.context.language === 'english' ? 'Name' : 'Naam';
     return (
       <div className="ui field">
         <label>{text}</label>
